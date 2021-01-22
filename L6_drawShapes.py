ac7e1e64f3334f5bd9f9ab4ff0a092f90be41ad8
jorge_Jetson <jorgeperez_1@live.com>
2021-01-22 16:54:40 -0600

    Drawing

--- L6_drawShapes.py
+++ L6_drawShapes.py
@@ -24,9 +24,15 @@ cv2.line(img, (0,0), (width, height) , (0,0,250), 2) #REd line
 img [120:200, 100:250] = 250, 0, 0
 
 # With openCV function
+# This one will be empty. If we leave a number as thickness
+# But we can set cv2.FILLED to do otherward
 cv2.rectangle(img, (300,150), (470,450), (250,0,0), 3)
 
+# Draw circle:
+cv2.circle(img, (430,250), 50, (255,255,0), 4)
 
+# Draw text
+cv2.putText(img, "Jorge!!", (250,50), cv2.FONT_HERSHEY_COMPLEX, 2, (255,255,255), 3)
 cv2.imshow("Figures", img)
 
 
